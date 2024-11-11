b5619a8b0d37a2e2f0ec15eb13af51de9de3612f
leandroptavares@gmail.com <leandroptavares@gmail.com>
2024-11-11 16:17:31 +0100

    3rd challenge done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,28 @@
+require "json"
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('a'..'z').to_a.sample(10)
+    # session[:letters] = @letters
+  end
+
+  def score
+    # raise
+    @letters = params[:letters].split(",")
+    @word = params[:word]
+    url = "https://dictionary.lewagon.com/#{params[:word]}"
+    score_serialized = URI.parse(url).read
+    score = JSON.parse(score_serialized)
+
+    if score["found"] == true
+      if @word.split("").all? { |letter|  @letters.include?(letter)}
+        @result = "Congratulations! #{@word.upcase} is a valid English word!"
+      else
+        @result = "Sorry but #{@word.upcase} can't be built with #{@letters.join(" ").upcase}"
+      end
+    else
+      @result = "Sorry but #{@word.upcase} does not seem to be a valid English word..."
+    end
+  end
+end
